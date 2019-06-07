c10b6a58f0d1d7a5cafd1c20469fbf4f0dedeca6
P Shyam Sundar <39958114+pssundaar@users.noreply.github.com>
2019-06-07 21:15:19 +0530

    Delete method2.py

--- How to convert RGB to Gray Scale Image/method2.py
+++
@@ -1,8 +0,0 @@
-import cv2
-img=cv2.imread('download.jpeg')
-cv2.imshow('output',img)
-cv2.waitKey(0)
-gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2.imshow('GRAY',gray_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
